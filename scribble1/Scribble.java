31356f69ff25d91c01826eee542a28f641419cf1
zzMOM <wuwei1107@gmail.com>
2013-10-20 15:01:13 -0700

    update

---
+++ scribble1/Scribble.java
@@ -0,0 +1,32 @@
+package scribble1;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Scribble extends JFrame{
+	protected ScribbleCanvas canvas;
+	
+	public Scribble(){
+		setTitle("Scribble Pad");
+		canvas = new ScribbleCanvas();
+		canvas.setBackground(Color.white);
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(canvas, BorderLayout.CENTER);
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+	}
+	
+	public static void main(String[] args){
+		int width = 600;
+		int height = 400;
+		JFrame frame = new Scribble();
+		frame.setSize(width, height);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation(screenSize.width/2 - width/2, screenSize.height/2 - height/2);
+		frame.show();
+	}
+}
