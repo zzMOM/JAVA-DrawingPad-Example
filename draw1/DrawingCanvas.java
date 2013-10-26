d1a1c8fd4815001afbfdbbb5526aa6d5c645efad
zzMOM <wuwei1107@gmail.com>
2013-10-25 20:32:44 -0700

    update DrawingCanvas

---
+++ draw1/DrawingCanvas.java
@@ -0,0 +1,26 @@
+package draw1;
+
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+import java.awt.event.*;
+import javax.swing.*;
+import scribble3.*;
+
+public class DrawingCanvas extends ScribbleCanvas{
+	protected DrawingCanvasListener drawingCanvasListener;
+
+	public DrawingCanvas(){}
+	
+	protected EventListener makeCanvasListener(){
+		return(drawingCanvasListener = new DrawingCanvasListener(this));
+	}
+	
+	public void setTool(Tool tool){
+		drawingCanvasListener.setTool(tool);
+	}
+	
+	public Tool getTool(){
+		return drawingCanvasListener.getTool();
+	}
+}
