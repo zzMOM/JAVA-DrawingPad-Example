98bbe44cc8819049db29e0acaac35227aec1fd74
zzMOM <wuwei1107@gmail.com>
2013-10-27 21:51:31 -0700

    update

--- draw1/LineShape.java
+++ draw1/LineShape.java
@@ -10,7 +10,7 @@ public class LineShape extends TwoEndsShape{
 		g.drawLine(x1, y1, x2, y2);
 	}
 	
-	public void drawOutLine(Graphics g, int x1, int x2, int y1, int y2){
+	public void drawOutline(Graphics g, int x1, int x2, int y1, int y2){
 		g.drawLine(x1, y1, x2, y2);
 	}
 }
