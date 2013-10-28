79c783952fda0116254c9698c759f5a7c960ae91
zzMOM <wuwei1107@gmail.com>
2013-10-27 21:27:55 -0700

    update

--- scribble3/Tool.java
+++ scribble3/Tool.java
@@ -1,9 +1,18 @@
 package scribble3;
 
 import java.awt.*;
-
+/**
+ * an interface is a group of related methods with empty bodies.
+ * Implementing an interface allows a class to become more formal about the behavior it promises to provide. 
+ * Interfaces form a contract between the class and the outside world, and this contract is enforced at build time by the compiler.
+ * If your class claims to implement an interface, 
+ * all methods defined by that interface must appear in its source code before the class will successfully compile.
+ * @author weiwu
+ *
+ */
 public interface Tool {
 	public String getName();
+	//Point p is from ScribbleCanvasListener-mousePressed in an instance class.
 	public void startShape(Point p);
 	public void addPointToShape(Point p);
 	public void endShape(Point p);
