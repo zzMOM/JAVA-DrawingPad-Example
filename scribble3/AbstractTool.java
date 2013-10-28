ea16b62fb4ac8e6a571b33b891e0df2cc31b42f6
zzMOM <wuwei1107@gmail.com>
2013-10-27 21:27:48 -0700

    update

--- scribble3/AbstractTool.java
+++ scribble3/AbstractTool.java
@@ -1,5 +1,11 @@
 package scribble3;
 
+/**
+ * getName() funciton are same for different tools.
+ * extends to this abstract class, other tools classes don't need to define getName() function anymore.
+ * @author weiwu
+ *
+ */
 public abstract class AbstractTool {
 	protected ScribbleCanvas canvas;
 	protected String name;
