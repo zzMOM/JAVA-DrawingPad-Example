a47b99a2856583aee2b175e8439cc4956eaedd8b
zzMOM <wuwei1107@gmail.com>
2013-11-01 21:37:30 -0700

    update canvas

--- draw4/ImageDrawingCanvasListener.java
+++ draw4/ImageDrawingCanvasListener.java
@@ -1,23 +1,25 @@
 package draw4;
 
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.net.URL;
 
 import draw1.DrawingCanvas;
 
 public class ImageDrawingCanvasListener extends draw3.KeyboardDrawingCanvasListener{
-	protected URL imageSrc;
+	protected File imageSrc;
 	
 	public ImageDrawingCanvasListener(DrawingCanvas canvas) { 
 		super(canvas); 
 	}
 	
-	public void setURL(URL imageSrc){
-		this.imageSrc = imageSrc;
-	}
-	
-	public URL getURL(){
-		return imageSrc;
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Point p = e.getPoint();
+		tool.startShape(p);
+		canvas.x = p.x;
+		canvas.y = p.y; 
 	}
 	
 }
