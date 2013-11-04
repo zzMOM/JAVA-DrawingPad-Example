0d7a38213ab6e7f3f98b5a30bb09bd0d44853795
zzMOM <wuwei1107@gmail.com>
2013-11-04 14:48:17 -0800

    update DrawingPad replace button text buy icon

--- draw4/DrawingPad.java
+++ draw4/DrawingPad.java
@@ -1,16 +1,21 @@
 package draw4;
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.*;
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.*;
 
 import scribble3.ScribbleCanvas;
+import scribble3.Tool;
 
 
 
@@ -22,6 +27,16 @@ public class DrawingPad extends draw3.DrawingPad{
 		super(title);
 		JMenu menu = menuBar.getMenu(1);
 		imageOptions(menu);
+		ActionListener toolListener = new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				Object source = event.getSource();
+				if(source instanceof AbstractButton){
+					AbstractButton button = (AbstractButton) source;
+					Tool tool = toolkit.setSelectedTool(button.getName());
+					drawingCanvas.setTool(tool);
+				}
+			}
+		};
 	}
 	
 	//factory method
@@ -31,8 +46,42 @@ public class DrawingPad extends draw3.DrawingPad{
 	
 	protected void initTools(){
 		super.initTools();
-		toolkit.addTool(new ImageTool(canvas, "Image"));
+        File imageSrc = new File("src/draw4/sun.jpg");
+		toolkit.addTool(new ImageTool(canvas, "Image", imageSrc));
 	}
+	
+	protected JComponent createToolBar(ActionListener toolListener){
+		JPanel toolbar = new JPanel(new GridLayout(0, 1));
+		int n = toolkit.getToolCount();
+		for(int i = 0; i < n; i++){
+			Tool tool = toolkit.getTool(i);
+			if(tool != null){
+				JButton button = new JButton();
+				try {
+					String name = tool.getName();
+					name = "src/img/" + name + ".ico";
+					File fileName = new File(name);
+				    Image img = ImageIO.read(fileName);
+				    button.setIcon(new ImageIcon(img));
+				    button.setName(tool.getName());
+				  } catch (IOException ex) {}
+				button.addActionListener(toolListener);
+				toolbar.add(button);
+			}
+		}
+		return toolbar;
+	}
+	
+	ActionListener toolListener = new ActionListener(){
+		public void actionPerformed(ActionEvent event){
+			Object source = event.getSource();
+			if(source instanceof AbstractButton){
+				AbstractButton button = (AbstractButton) source;
+				Tool tool = toolkit.setSelectedTool(button.getName());
+				drawingCanvas.setTool(tool);
+			}
+		}
+	};
 	
 	protected void imageOptions(JMenu optionMenu){
 		String[] imageNames ={"Sun", "Moon", "Star"};
@@ -42,7 +91,7 @@ public class DrawingPad extends draw3.DrawingPad{
 				if(source instanceof JCheckBoxMenuItem){
 					JCheckBoxMenuItem mi = (JCheckBoxMenuItem) source;
 					String name = mi.getText();
-					name = "draw4/" + name.toLowerCase() + ".jpg";
+					name = "src/draw4/" + name.toLowerCase() + ".jpg";
 					File imageSrc = null;
 			        //try {
 						imageSrc = new File(name);
