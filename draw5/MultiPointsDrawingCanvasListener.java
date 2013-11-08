79a85f75fb77ddc755dcf7dee7c663df9f309dd9
zzMOM <wuwei1107@gmail.com>
2013-11-08 10:25:01 -0800

    update

--- draw5/MultiPointsDrawingCanvasListener.java
+++ draw5/MultiPointsDrawingCanvasListener.java
@@ -7,23 +7,45 @@ import draw3.KeyboardDrawingCanvas;
 
 
 public class MultiPointsDrawingCanvasListener extends draw3.KeyboardDrawingCanvasListener{
+	protected PolygonTool polygonTool = (PolygonTool) tool; 
 	
 	protected MultiPointsDrawingCanvasListener(KeyboardDrawingCanvas canvas){
 		super(canvas);
 	}
 	
+	
+	public void mousePressed(MouseEvent e){
+		Point p = e.getPoint();
+		if(!(tool instanceof PolygonTool)){
+			tool.startShape(p);
+			canvas.mouseButtonDown = true;
+		}
+		canvas.x = p.x;
+		canvas.y = p.y;
+	}
+	
 	public void mouseReleased(MouseEvent e){
 		Point p = e.getPoint();
-		tool.endShape(p);
-		canvas.mouseButtonDown = false;
-		if (tool instanceof PolygonTool) {	
-			PolygonTool polygonTool = (PolygonTool) tool; 
+		if(tool instanceof PolygonTool){
 			polygonTool.addPointToArray(p);
 			if(e.getClickCount() == 2){
 				polygonTool.endArray(p);
 			}
+		} else {
+			tool.endShape(p);
+			canvas.mouseButtonDown = false;
 		}
-		
+	}
+	
+	public void mouseDragged(MouseEvent e){
+		Point p = e.getPoint();
+		if(!(tool instanceof PolygonTool)){
+			if(canvas.mouseButtonDown){
+				tool.addPointToShape(p);
+				canvas.x = p.x;
+				canvas.y = p.y;
+			}
+		}
 	}
 	
 
