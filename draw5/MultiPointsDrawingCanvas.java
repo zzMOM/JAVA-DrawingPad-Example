02c44676007f9a57db775e97b1a2a427c7d08766
zzMOM <wuwei1107@gmail.com>
2013-11-08 10:24:45 -0800

    update

--- draw5/MultiPointsDrawingCanvas.java
+++ draw5/MultiPointsDrawingCanvas.java
@@ -3,6 +3,7 @@ package draw5;
 import java.util.EventListener;
 
 import draw1.*;
+import draw3.KeyboardDrawingCanvasListener;
 
 public class MultiPointsDrawingCanvas extends draw3.KeyboardDrawingCanvas{
 	public MultiPointsDrawingCanvas() {}
