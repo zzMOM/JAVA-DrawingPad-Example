fa1e29e23621c36273363d8e8decda9e0731522a
zzMOM <wuwei1107@gmail.com>
2013-11-12 14:43:10 -0800

    update filled polygon

---
+++ draw6/FilledPolygonShape.java
@@ -0,0 +1,16 @@
+package draw6;
+
+import java.awt.Graphics;
+
+public class FilledPolygonShape extends draw5.MultiPointsShape{
+	public void draw(Graphics g){
+		if(color != null){
+			g.setColor(color);
+		}
+		g.fillPolygon(x, y, x.length);
+	}
+	
+	public void drawOutline(Graphics g, int x1, int y1, int x2, int y2){
+		g.drawLine(x1, y1, x2, y2);
+	}
+}
